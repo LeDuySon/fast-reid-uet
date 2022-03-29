eae99f172009ce8f901fbaf1939fb0303ae1edd2
Le Duy Son <leduyson2632001@gmail.com>
2022-03-29 21:53:34 +0700

    add build reid model for infer

---
+++ demo/uet_reid_infer.py
@@ -0,0 +1,52 @@
+# encoding: utf-8
+"""
+@author:  liaoxingyu
+@contact: sherlockliao01@gmail.com
+"""
+
+import argparse
+import glob
+import os
+import sys
+
+import torch.nn.functional as F
+import cv2
+import numpy as np
+import tqdm
+from torch.backends import cudnn
+
+sys.path.append('.')
+
+from fastreid.config import get_cfg
+from fastreid.utils.logger import setup_logger
+from fastreid.utils.file_io import PathManager
+
+from predictor import FeatureExtractionDemo
+
+# import some modules added in project like this below
+# sys.path.append("projects/PartialReID")
+# from partialreid import *
+
+cudnn.benchmark = True
+setup_logger(name="fastreid")
+
+def setup_cfg(args):
+    # load config from file and command-line arguments
+    cfg = get_cfg()
+    # add_partialreid_config(cfg)
+    cfg.merge_from_file(args.config_file)
+    cfg.merge_from_list(args.opts)
+    cfg.freeze()
+    return cfg
+
+def postprocess(features):
+    # Normalize feature to compute cosine distance
+    features = F.normalize(features)
+    features = features.cpu().data.numpy()
+    return features
+
+def build_model(args):
+    cfg = setup_cfg(args)
+    demo = FeatureExtractionDemo(cfg, parallel=args.parallel)
+    
+    return demo
